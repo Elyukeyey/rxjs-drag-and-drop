4ce5ac5fc019102513fa9586271193105bc178fa
Elyukeyey <lukam@nym.hush.com>
2019-06-30 09:37:11 +0200

    5 files modified, 2 deleted

--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -1,53 +1,88 @@
 import { Component, OnInit, Input, ViewChild, ElementRef, EventEmitter } from '@angular/core';
-import { Observable, fromEvent } from 'rxjs';
-import { mergeMap, takeUntil, repeat } from 'rxjs/operators';
+import { Observable, fromEvent, of } from 'rxjs';
+import { mergeMap, takeUntil, skipUntil, repeat } from 'rxjs/operators';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
+  template: `
+  <div class="square" [style.top]="position.top" [style.left]="position.left"
+   [style.cursor]="cursor" [style.transform]="position.transform" #moveSquare>
+      <span class="middle" #faceSpan>😏</span>
+  </div>
+  `,
   styleUrls: ['./square.component.css']
 })
 export class SquareComponent implements OnInit {
+  // element Selectors
+  @ViewChild('moveSquare', { static: true}) element: ElementRef;
+  @ViewChild('faceSpan', {static: true}) faceSpan: ElementRef;
+  
+  // Observables 
   down$: Observable<any>;
   up$: Observable<any>;
-  move$: Observable<Object>; // { posX: , posY: }
-  @ViewChild('moveSquare') element: ElementRef;
-  @ViewChild('faceSpan') faceSpan: ElementRef;
+  move$: Observable<Object>;
+  leave$: Observable<any>;
+  
+  // cursor position in the element;
   startX: number;
   startY: number;
+
+  // starting position in the center of the screen
+  position = { top: '50%', left: '50%', transform: 'translate(-50%, -50%)'};
+
   cursor: string;
-  position: { top: string, left: string};
+  
 
   constructor() { }
 
   ngOnInit() {
-    this.position = { top: '100px', left: '100px' }
     this.move$ = fromEvent(this.element.nativeElement, 'mousemove');
     this.down$ = fromEvent(this.element.nativeElement, 'mousedown');
     this.up$ = fromEvent(this.element.nativeElement, 'mouseup');
+    this.leave$ = fromEvent(this.element.nativeElement, 'mouseleave');
+
 
     this.down$.pipe(
       mergeMap(down => this.move$.pipe(
-        takeUntil(this.up$)
+        takeUntil(this.up$),
+        takeUntil(this.leave$)
         )
       )
     )
-    .subscribe(({ clientX, clientY }) => {
-      this.position = { top: clientY - this.startY + 'px', left: clientX - this.startX + 'px' };
+    .subscribe(({ clientX, clientY }: any ) => {
+      this.position = { top: clientY - this.startY + 'px', left: clientX - this.startX + 'px', transform: '' };
+      this._grab();
     });
-    
 
     this.down$.subscribe(({offsetX, offsetY}) => {
-      this.faceSpan.nativeElement.innerText = '😖';
-      this.cursor = 'grabbing';
       this.startX = offsetX;
       this.startY = offsetY;
     });
+
+    
+    // for fun only:
     this.up$.subscribe(() => {
-      this.faceSpan.nativeElement.innerText = '🤔';
-      this.cursor = 'grab'
-      
-      });
+      this._letGo();
+    });
+    this.leave$.pipe(
+      skipUntil(this.down$),
+      takeUntil(this.up$),
+      repeat()
+    ).subscribe(() => {
+      this._letGo()  
+    });
+
+  }
+
+  // for fun only, not cruicial to the proper functioning
+  _letGo = (): void => {
+    this.faceSpan.nativeElement.innerText = '🤬';
+    setTimeout(() => this.faceSpan.nativeElement.innerText = '🤔', 500);
+    this.cursor = 'grab';
+  }
+  _grab = (): void => {
+    this.faceSpan.nativeElement.innerText = '😖';
+    this.cursor = 'grabbing';
   }
 
 }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,8 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  template: `
+  <header>
+  	<h1>Drag & drop</h1>
+    <p>with RxJS and Angular</p>
+  </header>
+  `,
+  styleUrls: ['./header.component.css'],
+  styles: [`header {
+      width: 100%;
+      text-align: center;
+  }`]
 })
 export class HeaderComponent implements OnInit {
 
